8d183a47c02950031558d9f1a2ece98655027073
Juan Luna Ramirez <fclunar@gmail.com>
2015-02-08 11:17:09 -0800

    added Contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
   root to: 'welcome#index'
   #root is a method that takes a hash as a an argument
   #  root({to: 'welcome#index'})
