ac1e171cf8123d41b18d1d44a9e0e776148f2027
Juan Luna Ramirez <fclunar@gmail.com>
2015-02-28 23:37:01 -0800

    setup voting for posts

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
 
   resources :posts, only: [] do # [] creates /posts/:post_id/comments and prevents posts/:id
     resources :comments, only: [:create, :destroy]
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
 
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 
 describe Vote do
-  
+
   describe "validations" do
+
+
     describe "value validation" do
       it "only allow -1 or 1 as value" do
-        expect (Vote.new(value: 1).to eq(true)
-        expect (Vote.new(value: -1).to eq(true)
-        expect (Vote.new(value: 2).to eq(false)
+        up_vote = Vote.new(value: 1)
+        expect( up_vote.valid? ).to eq(true)
+
+        down_vote = Vote.new(value: -1)
+         expect( down_vote.valid? ).to eq(true)
+
+        invalid_vote = Vote.new(value: 2)      
+        expect( invalid_vote.valid? ).to eq(false)
       end
     end
-  end
 
+
+  end
 end
