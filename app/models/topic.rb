c58a6f3ac282760b3178458cd5a12e4fdbc55506
Juan Luna Ramirez <fclunar@gmail.com>
2015-03-05 19:56:29 -0800

    private topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  scope :visible_to, -> (user) { user ? all : where(public: true) } #if else statement in brackets {if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this}
+
 end
